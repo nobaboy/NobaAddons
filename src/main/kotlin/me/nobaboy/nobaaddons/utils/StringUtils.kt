cc258dc83e39c759a5bef9a9a7631cba4a2003a3
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-10-06 19:57:53 +0300

    Fix: missing space

--- src/main/kotlin/me/nobaboy/nobaaddons/utils/StringUtils.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/utils/StringUtils.kt
@@ -9,7 +9,7 @@ object StringUtils {
 	fun String.startsWith(list: List<String>): Boolean = list.any { this.startsWith(it) }
 
 	fun String.capitalizeFirstLetters(): String {
-		return this.split(" ").joinToString("") {
+		return this.split(" ").joinToString(" ") {
 			if(it.contains("/")) {
 				it.split("/").joinToString("/") { word ->
 					word.replaceFirstChar { firstChar -> firstChar.uppercase() }
