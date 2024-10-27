461e993dd39dc88e0a1bb018c60ae6875ff728f2
celeste <hi@celestialfault.dev>
2024-10-26 18:36:32 -0600

    use cooldowns for command queueing

---
+++ src/main/kotlin/me/nobaboy/nobaaddons/utils/CooldownTickEvent.kt
@@ -0,0 +1,51 @@
+package me.nobaboy.nobaaddons.utils
+
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.event.EventFactory
+import net.minecraft.client.MinecraftClient
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * [ClientTickEvents.END_CLIENT_TICK] but with a cooldown attached to it.
+ *
+ * ## Example
+ *
+ * ```kt
+ * object Event : CooldownTickEvent {
+ *     override fun onTick(client: MinecraftClient) {
+ *     	   // ... do something you want to guard with a cooldown ...
+ *     	   cooldownManager.startCooldown(30.ticks)
+ *     	   // now this event will be skipped for the next (about) 30 ticks, or 1.5 seconds
+ *     }
+ * }
+ * ```
+ */
+fun interface CooldownTickEvent {
+	fun onTick(client: MinecraftClient)
+
+	// this whole mutable map deal feels janky, but there isn't much else we can do here while working within the
+	// constraints of the fabric event system requiring a functional interface.
+	val cooldownManager: CooldownManager
+		get() = COOLDOWN_MANAGERS.getOrPut(this) { CooldownManager() }
+
+	companion object {
+		private val COOLDOWN_MANAGERS = mutableMapOf<CooldownTickEvent, CooldownManager>()
+
+		init {
+			ClientTickEvents.END_CLIENT_TICK.register { EVENT.invoker().onTick(it) }
+		}
+
+		val EVENT = EventFactory.createArrayBacked(CooldownTickEvent::class.java) { listeners ->
+			CooldownTickEvent { client ->
+				listeners.forEach {
+					if(it.cooldownManager.isOnCooldown()) return@forEach
+					it.onTick(client)
+				}
+			}
+		}
+
+		inline val Int.ticks: Duration
+			get() = (this / 20.0).seconds
+	}
+}
