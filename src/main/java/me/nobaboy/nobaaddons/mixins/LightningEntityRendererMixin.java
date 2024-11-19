39858fab232be1264bdea7906c1dcaeeb44b1097
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-11-19 18:06:11 +0300

    Rename mixin function so it resembles the feature it's for

--- src/main/java/me/nobaboy/nobaaddons/mixins/LightningEntityRendererMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/LightningEntityRendererMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LightningEntityRenderer.class)
 public class LightningEntityRendererMixin {
 	@Inject(method = "drawBranch", at = @At("HEAD"), cancellable = true)
-	private static void nobaaddons$onDrawBranch(
+	private static void nobaaddons$hideLightningBolts(
 		Matrix4f matrix,
 		VertexConsumer buffer,
 		float x1,
