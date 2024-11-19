92c9525ceb2392b0c332f0a67c2df54f411aaaa9
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-11-19 18:06:58 +0300

    I have to be careful with spelling.

--- src/main/java/me/nobaboy/nobaaddons/mixins/LightningEntityRendererMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/LightningEntityRendererMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LightningEntityRenderer.class)
 public class LightningEntityRendererMixin {
 	@Inject(method = "drawBranch", at = @At("HEAD"), cancellable = true)
-	private static void nobaaddons$hideLightningBolts(
+	private static void nobaaddons$hideLightningBolt(
 		Matrix4f matrix,
 		VertexConsumer buffer,
 		float x1,
