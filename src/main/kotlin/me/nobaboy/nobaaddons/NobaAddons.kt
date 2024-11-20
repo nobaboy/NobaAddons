51142e389b404676ad749e8fe1f4314ef4ec74e2
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-11-20 04:42:00 +0300

    Add mouse lock

--- src/main/kotlin/me/nobaboy/nobaaddons/NobaAddons.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/NobaAddons.kt
@@ -18,6 +18,7 @@ import me.nobaboy.nobaaddons.features.dungeons.SimonSaysTimer
 import me.nobaboy.nobaaddons.features.keybinds.KeyBindListener
 import me.nobaboy.nobaaddons.features.mining.glacitemineshaft.CorpseLocator
 import me.nobaboy.nobaaddons.features.mining.glacitemineshaft.MineshaftWaypoints
+import me.nobaboy.nobaaddons.features.qol.MouseLock
 import me.nobaboy.nobaaddons.features.qol.sound.filters.ISoundFilter
 import me.nobaboy.nobaaddons.features.visuals.TemporaryWaypoint
 import me.nobaboy.nobaaddons.features.visuals.itemoverlays.EtherwarpHelper
@@ -103,6 +104,7 @@ object NobaAddons : ClientModInitializer {
 
 		// region QOL
 		ISoundFilter.init()
+		MouseLock.init()
 		// endregion
 		/* endregion */
 	}
