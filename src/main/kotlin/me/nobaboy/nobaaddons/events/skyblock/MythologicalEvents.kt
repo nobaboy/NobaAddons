9b58cc9fe79ed5194868216f82ba776916d28d0e
celeste <hi@celestialfault.dev>
2024-11-23 14:27:47 -0700

    cleanup mythological code
    adds a clearburrows command as a band-aid fix for frequent ghost mob
    burrows - this should ideally be properly fixed better later, but I'm
    not about to try to do that myself.
    
    use the existing heightmap predicate instead of trying to roll our own
    (I would've liked to use the existing code for this, but I don't think
    Hypixel sends the data necessary for that?)
    
    fix inquisitor waypoints never actually being created because of an
    incorrect `is` check on `ServerPlayerEntity`, which is - in fact - not
    used by the client on a multiplayer server.

--- src/main/kotlin/me/nobaboy/nobaaddons/events/skyblock/MythologicalEvents.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/events/skyblock/MythologicalEvents.kt
@@ -3,7 +3,7 @@ package me.nobaboy.nobaaddons.events.skyblock
 import me.nobaboy.nobaaddons.events.internal.EventDispatcher
 import me.nobaboy.nobaaddons.features.events.mythological.BurrowType
 import me.nobaboy.nobaaddons.utils.NobaVec
-import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.client.network.OtherClientPlayerEntity
 
 object MythologicalEvents {
 	@JvmField
@@ -21,5 +21,5 @@ object MythologicalEvents {
 	data class BurrowGuess(val location: NobaVec)
 	data class BurrowFind(val location: NobaVec, val type: BurrowType)
 	data class BurrowDig(val location: NobaVec)
-	data class InquisitorSpawn(val entity: ServerPlayerEntity)
+	data class InquisitorSpawn(val entity: OtherClientPlayerEntity)
 }
