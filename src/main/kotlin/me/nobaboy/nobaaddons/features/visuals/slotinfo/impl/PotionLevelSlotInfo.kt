8b3e6db13258674a154aaaacaaa84b3e541bb4d7
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-11-23 05:21:30 +0300

    Fixed healer class item drawing a count

--- src/main/kotlin/me/nobaboy/nobaaddons/features/visuals/slotinfo/impl/PotionLevelSlotInfo.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/features/visuals/slotinfo/impl/PotionLevelSlotInfo.kt
@@ -8,7 +8,10 @@ object PotionLevelSlotInfo : ISlotInfo {
 	override val enabled: Boolean get() = config.potionLevel
 
 	override fun handle(event: ScreenRenderEvents.DrawSlot) {
-		val item = event.itemStack.getSkyBlockItem() ?: return
+		val itemStack = event.itemStack
+		if(itemStack.name.string.startsWith("Healer")) return
+
+		val item = itemStack.getSkyBlockItem() ?: return
 		if(item.id != "POTION") return
 
 		val level = item.potionLevel.toString()
