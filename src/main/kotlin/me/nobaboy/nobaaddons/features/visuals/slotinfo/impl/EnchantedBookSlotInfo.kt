a588fe1e0c1d5f35de44c81b2a281d950deacfe7
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-11-27 16:41:41 +0300

    Temporary solution for enchants with wrong id

--- src/main/kotlin/me/nobaboy/nobaaddons/features/visuals/slotinfo/impl/EnchantedBookSlotInfo.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/features/visuals/slotinfo/impl/EnchantedBookSlotInfo.kt
@@ -5,36 +5,44 @@ import me.nobaboy.nobaaddons.features.visuals.slotinfo.ISlotInfo
 import me.nobaboy.nobaaddons.utils.StringUtils.title
 import me.nobaboy.nobaaddons.utils.TextUtils.buildText
 import me.nobaboy.nobaaddons.utils.items.ItemUtils.getSkyBlockItem
-import me.nobaboy.nobaaddons.utils.items.SkyBlockItemData
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 
 object EnchantedBookSlotInfo : ISlotInfo {
 	override val enabled: Boolean get() = config.enchantedBookLevel || config.enchantedBookName
 
+	private val correctEnchantIds = mutableMapOf<String, String>(
+		"aiming" to "dragon_tracer",
+		"ultimate_reiterate" to "ultimate_duplex",
+		"ultimate_wise" to "ultimate_ultimate_wise",
+		"ultimate_jerry" to "ultimate_ultimate_jerry"
+	)
+
 	override fun handle(event: ScreenRenderEvents.DrawSlot) {
 		val item = event.itemStack.getSkyBlockItem() ?: return
 		if(item.id != "ENCHANTED_BOOK" || item.enchantments.size != 1) return
 
 		if(config.enchantedBookLevel) drawCount(event, item.enchantments.values.first().toString())
-		if(config.enchantedBookName) drawInfo(event, formatEnchantName(item))
+		if(config.enchantedBookName) {
+			val enchantId = item.enchantments.keys.first()
+			drawInfo(event, formatEnchantName(enchantId))
+		}
 	}
 
-	private fun formatEnchantName(item: SkyBlockItemData): Text {
-		val enchant = item.enchantments.keys.first()
+	private fun formatEnchantName(id: String): Text {
+		val enchantId = correctEnchantIds[id] ?: id
 
 		val text = buildText {
 			when {
-				enchant.startsWith("ultimate_") -> {
-					val name = if(enchant.endsWith("wise") || enchant.endsWith("jerry")) enchant else enchant.removePrefix("ultimate_")
+				enchantId.startsWith("ultimate_") -> {
 					formatted(Formatting.LIGHT_PURPLE, Formatting.BOLD)
-					append(formatShortenedName(name))
+					append(formatShortenedName(enchantId.removePrefix("ultimate_")))
 				}
-				enchant.startsWith("turbo_") -> {
-					append(formatShortenedName(enchant.removePrefix("turbo_")))
+				enchantId.startsWith("turbo_") -> {
+					append(formatShortenedName(enchantId.removePrefix("turbo_")))
 				}
 				else -> {
-					append(formatShortenedName(enchant))
+					append(formatShortenedName(enchantId))
 				}
 			}
 		}
