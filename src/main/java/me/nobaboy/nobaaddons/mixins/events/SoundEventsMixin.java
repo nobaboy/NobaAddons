359251abb0c8a171875fedc636d9b03bccab21a0
celeste <hi@celestialfault.dev>
2024-12-01 16:37:52 -0700

    use WrapWithCondition instead

--- src/main/java/me/nobaboy/nobaaddons/mixins/events/SoundEventsMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/events/SoundEventsMixin.java
@@ -1,25 +1,27 @@
 package me.nobaboy.nobaaddons.mixins.events;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import me.nobaboy.nobaaddons.events.SoundEvents;
 import me.nobaboy.nobaaddons.utils.NobaVec;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.PlaySoundS2CPacket;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class SoundEventsMixin {
-	@Inject(
+	@WrapWithCondition(
 		method = "onPlaySound",
-		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;playSound(Lnet/minecraft/entity/player/PlayerEntity;DDDLnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/sound/SoundCategory;FFJ)V"),
-		cancellable = true
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;playSound(Lnet/minecraft/entity/player/PlayerEntity;DDDLnet/minecraft/registry/entry/RegistryEntry;Lnet/minecraft/sound/SoundCategory;FFJ)V")
 	)
-	public void nobaaddons$onPlaySound(PlaySoundS2CPacket packet, CallbackInfo ci) {
-		var sound = packet.getSound().getKeyOrValue();
+	public boolean nobaaddons$onPlaySound(ClientWorld instance, @Nullable PlayerEntity source, double x, double y, double z, RegistryEntry<SoundEvent> soundKey, SoundCategory category, float volume, float pitch, long seed) {
+		var sound = soundKey.getKeyOrValue();
 		var id = sound.left()
 			.map(RegistryKey::getValue)
 			.orElseGet(() -> sound.right()
@@ -30,17 +32,17 @@ public class SoundEventsMixin {
 				//?}
 				.orElseThrow());
 
-		var location = new NobaVec(packet.getX(), packet.getY(), packet.getZ());
+		var location = new NobaVec(x, y, z);
 
-		var allow = new SoundEvents.AllowSound(id, location, packet.getPitch(), packet.getVolume());
-		var soundEvent = new SoundEvents.Sound(id, packet.getCategory(), location, packet.getPitch(), packet.getVolume());
+		var allow = new SoundEvents.AllowSound(id, location, pitch, volume);
+		var soundEvent = new SoundEvents.Sound(id, category, location, pitch, volume);
 		SoundEvents.ALLOW_SOUND.invoke(allow);
 		if(allow.isCanceled()) {
 			SoundEvents.SOUND_CANCELED.invoke(soundEvent);
-			ci.cancel();
-			return;
+			return false;
 		}
 
 		SoundEvents.SOUND.invoke(soundEvent);
+		return true;
 	}
 }
