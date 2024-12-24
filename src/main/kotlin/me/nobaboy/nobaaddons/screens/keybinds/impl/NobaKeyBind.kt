688e62d24d02ab7cbe1c6cbd42c526378f5445d5
celeste <hi@celestialfault.dev>
2024-12-23 17:02:25 -0700

    and the final one

--- src/main/kotlin/me/nobaboy/nobaaddons/screens/keybinds/impl/NobaKeyBind.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/screens/keybinds/impl/NobaKeyBind.kt
@@ -3,6 +3,8 @@ package me.nobaboy.nobaaddons.screens.keybinds.impl
 import me.nobaboy.nobaaddons.utils.CooldownManager
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.util.InputUtil
+import net.minecraft.text.Text
+import net.minecraft.text.TranslatableTextContent
 import org.lwjgl.glfw.GLFW
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -14,6 +16,24 @@ open class NobaKeyBind(
 	private val onPress: () -> Unit
 ) : KeyBinding(name, inputType, key, category) {
 	private val cooldownManager = CooldownManager()
+
+	/**
+	 * Override to allow for mc-auto-translations to automatically discover translation strings
+	 * without needing to manually register it
+	 */
+	constructor(
+		name: Text,
+		category: String = "nobaaddons.name",
+		key: Int = GLFW.GLFW_KEY_UNKNOWN,
+		inputType: InputUtil.Type = InputUtil.Type.KEYSYM,
+		onPress: () -> Unit,
+	) : this(
+		(name.content as TranslatableTextContent).key,
+		category,
+		key,
+		inputType,
+		onPress,
+	)
 
 	@Suppress("UsePropertyAccessSyntax") // using property access syntax causes a StackOverflowError
 	override fun setPressed(pressed: Boolean) {
