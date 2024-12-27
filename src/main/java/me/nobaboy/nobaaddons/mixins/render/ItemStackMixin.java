2ab5773a53a6f52c49bacb252df0d878e9555149
nobaboy <84668101+nobaboy@users.noreply.github.com>
2024-12-27 15:58:09 +0300

    Merge branch 'main' into feature/slayer

--- src/main/java/me/nobaboy/nobaaddons/mixins/render/ItemStackMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/render/ItemStackMixin.java
@@ -11,10 +11,6 @@ import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-/*? if <1.21.2 {*/
-/*import net.minecraft.item.ArmorItem;*/
-/*?}*/
-
 @Mixin(ItemStack.class)
 abstract class ItemStackMixin {
 	@ModifyReturnValue(method = "hasGlint", at = @At("RETURN"))
