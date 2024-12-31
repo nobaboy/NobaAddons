ab9de06a6271729a5f635066d4fac20396cd112b
celeste <hi@celestialfault.dev>
2024-12-31 02:08:48 -0700

    use our coroutine scope

--- src/main/kotlin/me/nobaboy/nobaaddons/commands/impl/NobaClientCommand.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/commands/impl/NobaClientCommand.kt
@@ -2,6 +2,10 @@ package me.nobaboy.nobaaddons.commands.impl
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException
 import dev.celestialfault.commander.client.ClientCommand
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import me.nobaboy.nobaaddons.NobaAddons
 import me.nobaboy.nobaaddons.utils.ErrorManager
 import kotlin.reflect.KFunction
 
@@ -10,4 +14,6 @@ class NobaClientCommand(function: KFunction<*>, instance: Any?) : ClientCommand(
 		if(error is CommandSyntaxException) throw error
 		ErrorManager.logError("Command '$name' threw an unhandled error", error, ignorePreviousErrors = true)
 	}
+
+	override fun runAsync(runnable: suspend (CoroutineScope) -> Unit): Job = NobaAddons.coroutineScope.launch(block = runnable)
 }
