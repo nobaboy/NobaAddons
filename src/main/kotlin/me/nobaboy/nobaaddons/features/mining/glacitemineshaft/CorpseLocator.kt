258d294657d8075d7b863f0d870ca21adb636dac
nobaboy <84668101+nobaboy@users.noreply.github.com>
2025-01-04 05:10:57 +0300

    I really don't understand the big deal with hardcoded strings but whatever :shrug:

--- src/main/kotlin/me/nobaboy/nobaaddons/features/mining/glacitemineshaft/CorpseLocator.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/features/mining/glacitemineshaft/CorpseLocator.kt
@@ -73,8 +73,7 @@ object CorpseLocator {
 			val location = corpse.entity.getNobaVec().roundToBlock()
 			val (x, y, z) = location.toDoubleArray().map { it.toInt() }
 
-			val article = if(corpse.type == CorpseType.UMBER) "an" else "a"
-			val text = tr("nobaaddons.corpseLocator.found", "Found $article ${corpse.type.formattedDisplayName} at $x, $y, $z!")
+			val text = tr("nobaaddons.corpseLocator.found", "Found ${corpse.type.formattedDisplayName} at $x, $y, $z!")
 
 			ChatUtils.addMessage(text)
 			MineshaftWaypoints.addWaypoint(location, corpse.type.displayName, corpse.type.color, MineshaftWaypointType.CORPSE)
