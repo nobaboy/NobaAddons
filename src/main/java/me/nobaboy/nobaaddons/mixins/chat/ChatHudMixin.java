08811e36ea5465d9a98c5d4c8c268a456f145d60
celeste <hi@celestialfault.dev>
2025-01-10 01:15:04 -0700

    remove unused import

--- src/main/java/me/nobaboy/nobaaddons/mixins/chat/ChatHudMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/chat/ChatHudMixin.java
@@ -16,8 +16,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-
 @Mixin(ChatHud.class)
 abstract class ChatHudMixin {
 	@Unique private final ThreadLocal<@Nullable ChatHudLine> LINE = new ThreadLocal<>();
