43cb05e80acd3e3aaff04bc967dedf43a97c8f6b
celeste <hi@celestialfault.dev>
2025-03-12 17:29:02 -0600

    use tags for crimson isle mythic catch alert

--- src/main/kotlin/me/nobaboy/nobaaddons/core/fishing/SeaCreature.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/core/fishing/SeaCreature.kt
@@ -8,14 +8,42 @@ import me.nobaboy.nobaaddons.utils.StringUtils.title
 
 @Serializable
 data class SeaCreature(
+	/**
+	 * Internal ID of the sea creature
+	 */
 	val id: String,
+	/**
+	 * Optional name override; this defaults to the title-cased value of [id] if not provided.
+	 */
 	val name: String? = null,
+	/**
+	 * The chat message sent when this sea creature is caught
+	 */
 	val spawnMessage: String,
+	/**
+	 * If `true`, [spawnMessage] will be treated as a [Regex] pattern
+	 */
 	val spawnMessageRegex: Boolean = false,
+	/**
+	 * The [Rarity] of this sea creature
+	 */
 	val rarity: Rarity,
+	/**
+	 * All possible [SkyBlockIsland]s that this creature can be caught on
+	 */
 	val islands: List<SkyBlockIsland> = emptyList(),
+	/**
+	 * The possible zones that this creature can be caught in
+	 */
 	val zones: List<String>? = null,
+	/**
+	 * Whether this creature is caught from water(/ice) or lava fishing
+	 */
 	val type: SeaCreatureType = SeaCreatureType.WATER,
+	/**
+	 * Internal tags used by various features
+	 */
+	val tags: List<String> = emptyList(),
 ) {
 	val displayName: String by lazy { name ?: id.replace("_", " ").title() }
 
