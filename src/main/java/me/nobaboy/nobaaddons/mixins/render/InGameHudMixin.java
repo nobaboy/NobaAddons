3f5624f03930a2f9a6e0fe2f57e424b29ef815ef
celeste <hi@celestialfault.dev>
2025-03-17 12:28:06 -0600

    only hide in skyblock

--- src/main/java/me/nobaboy/nobaaddons/mixins/render/InGameHudMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/render/InGameHudMixin.java
@@ -5,6 +5,7 @@ package me.nobaboy.nobaaddons.mixins.render;
 //?}
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import me.nobaboy.nobaaddons.api.skyblock.SkyBlockAPI;
 import me.nobaboy.nobaaddons.config.NobaConfig;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
@@ -30,12 +31,15 @@ abstract class InGameHudMixin {
 	)
 	public boolean nobaaddons$hideAirBubbles(DrawContext instance, Identifier texture, int x, int y, int width, int height) {
 	*///?}
-		return !NobaConfig.INSTANCE.getUiAndVisuals().getRenderingTweaks().getHideAirBubbles();
+		if(SkyBlockAPI.inSkyBlock()) {
+			return !NobaConfig.INSTANCE.getUiAndVisuals().getRenderingTweaks().getHideAirBubbles();
+		}
+		return true;
 	}
 
 	@WrapWithCondition(method = "renderHealthBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawHeart(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/client/gui/hud/InGameHud$HeartType;IIZZZ)V"))
 	public boolean nobaaddons$hideAbsorptionHearts(InGameHud instance, DrawContext context, InGameHud.HeartType type, int x, int y, boolean hardcore, boolean blinking, boolean half) {
-		if(NobaConfig.INSTANCE.getUiAndVisuals().getRenderingTweaks().getHideAbsorptionHearts()) {
+		if(SkyBlockAPI.inSkyBlock() && NobaConfig.INSTANCE.getUiAndVisuals().getRenderingTweaks().getHideAbsorptionHearts()) {
 			return type != InGameHud.HeartType.ABSORBING;
 		}
 		return true;
