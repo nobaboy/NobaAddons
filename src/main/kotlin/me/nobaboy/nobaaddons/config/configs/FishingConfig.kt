16a9e012dd53acdee374183f5c77705a272ef869
celeste <hi@celestialfault.dev>
2025-03-23 11:11:37 -0600

    add feature to revert treasure catch colors (#97)

--- src/main/kotlin/me/nobaboy/nobaaddons/config/configs/FishingConfig.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/config/configs/FishingConfig.kt
@@ -15,6 +15,7 @@ class FishingConfig : ObjectProperty<FishingConfig>("fishing") {
 	val seaCreatureAlert by SeaCreatureAlert()
 	val bobberTimer by BobberTimer()
 	val trophyFishing by TrophyFishing()
+	val catchMessages by CatchMessages()
 	val highlightThunderSparks by HighlightThunderSparks()
 
 	class SeaCreatureAlert : ObjectProperty<SeaCreatureAlert>("seaCreatureAlert") {
@@ -35,6 +36,11 @@ class FishingConfig : ObjectProperty<FishingConfig>("fishing") {
 		var modifyChatMessages by Property.of<Boolean>("modifyChatMessages", false)
 		var compactMessages by Property.of<Boolean>("compactMessages", false)
 		var compactMaxRarity by Property.of<TrophyFishRarity>("compactMaxRarity", Serializer.enum(), TrophyFishRarity.SILVER)
+	}
+
+	class CatchMessages : ObjectProperty<CatchMessages>("catchMessages") {
+		var revertTreasureMessages by Property.of<Boolean>("revertTreasureMessages", false)
+		// TODO add a custom sea creature catch message formatting, like what skyhanni has
 	}
 
 	class HighlightThunderSparks : ObjectProperty<HighlightThunderSparks>("highlightThunderSparks") {
