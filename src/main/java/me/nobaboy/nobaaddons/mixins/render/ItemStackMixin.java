3a615eda5e5d9608f3569d26f8bec4386000f27b
celeste <hi@celestialfault.dev>
2025-04-01 13:54:32 -0600

    what the fuck

--- src/main/java/me/nobaboy/nobaaddons/mixins/render/ItemStackMixin.java
+++ src/main/java/me/nobaboy/nobaaddons/mixins/render/ItemStackMixin.java
@@ -1,21 +1,20 @@
 package me.nobaboy.nobaaddons.mixins.render;
 
-//? if >=1.21.5 {
-/*import net.minecraft.component.type.TooltipDisplayComponent;
-import net.minecraft.entity.player.PlayerEntity;
-*///?} else {
-import net.minecraft.component.ComponentType;
-import org.spongepowered.asm.mixin.injection.Slice;
-//?}
-
 //? if <1.21.2 {
 /*import net.minecraft.item.ArmorItem;
+*///?}
+
+//? if <1.21.5 {
+import org.spongepowered.asm.mixin.injection.Slice;
+//?} else {
+/*import net.minecraft.component.type.TooltipDisplayComponent;
 *///?}
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import me.nobaboy.nobaaddons.api.skyblock.SkyBlockAPI;
 import me.nobaboy.nobaaddons.utils.items.ItemUtils;
+import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -26,7 +25,6 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.function.Consumer;
 
-// FIXME do not ask why this is like this, I just wanted to game to load.
 @Mixin(ItemStack.class)
 abstract class ItemStackMixin {
 	@ModifyReturnValue(method = "hasGlint", at = @At("RETURN"))
@@ -39,11 +37,15 @@ abstract class ItemStackMixin {
 	}
 
 	@WrapWithCondition(
+		//? if >=1.21.5 {
+		/*method = "appendTooltip",
+		*///?} else {
 		method = "getTooltip",
+		//?}
 		at = @At(
 			value = "INVOKE",
 			//? if >=1.21.5 {
-			/*target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/item/Item$TooltipContext;Lnet/minecraft/component/type/TooltipDisplayComponent;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/tooltip/TooltipType;Ljava/util/function/Consumer;)V"
+			/*target = "Lnet/minecraft/item/ItemStack;appendComponentTooltip(Lnet/minecraft/component/ComponentType;Lnet/minecraft/item/Item$TooltipContext;Lnet/minecraft/component/type/TooltipDisplayComponent;Ljava/util/function/Consumer;Lnet/minecraft/item/tooltip/TooltipType;)V"
 			*///?} else {
 			target = "Lnet/minecraft/item/ItemStack;appendTooltip(Lnet/minecraft/component/ComponentType;Lnet/minecraft/item/Item$TooltipContext;Ljava/util/function/Consumer;Lnet/minecraft/item/tooltip/TooltipType;)V"
 			//?}
@@ -56,25 +58,15 @@ abstract class ItemStackMixin {
 	)
 	public boolean nobaaddons$removeVanillaEnchants(
 		ItemStack instance,
-		//? if <1.21.5 {
 		ComponentType<?> componentType,
-		//?}
 		Item.TooltipContext context,
 		//? if >=1.21.5 {
-		/*TooltipDisplayComponent tooltipDisplayComponent,
-		PlayerEntity playerEntity,
-		TooltipType tooltipType,
-		Consumer<Text> consumer
-		*///?} else {
-		Consumer<Text> consumer,
+		/*TooltipDisplayComponent displayComponent,
+		*///?}
+		Consumer<Text> textConsumer,
 		TooltipType type
-		//?}
 	) {
-		//? if >=1.21.5 {
-		/*if(!tooltipDisplayComponent.hiddenComponents().contains(DataComponentTypes.STORED_ENCHANTMENTS) && !tooltipDisplayComponent.hiddenComponents().contains(DataComponentTypes.ENCHANTMENTS)) return true;
-		*///?} else {
 		if(componentType != DataComponentTypes.STORED_ENCHANTMENTS && componentType != DataComponentTypes.ENCHANTMENTS) return true;
-		//?}
 		if(!SkyBlockAPI.inSkyBlock()) return true;
 		return !ItemUtils.isSkyBlockItem((ItemStack) (Object) this);
 	}
