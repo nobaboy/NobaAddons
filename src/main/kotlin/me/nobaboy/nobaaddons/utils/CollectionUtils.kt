80066bdd9d1bdc5208444824130a4ba264baeb93
nobaboy <84668101+nobaboy@users.noreply.github.com>
2025-04-04 07:29:46 +0300

    Add return type

--- src/main/kotlin/me/nobaboy/nobaaddons/utils/CollectionUtils.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/utils/CollectionUtils.kt
@@ -1,12 +1,14 @@
 package me.nobaboy.nobaaddons.utils
 
 object CollectionUtils {
-	fun List<String>.anyEquals(other: String, ignoreCase: Boolean = false) = this.any { it.equals(other, ignoreCase = ignoreCase) }
-	fun List<String>.anyContains(other: String, ignoreCase: Boolean = false) = this.any { it.contains(other, ignoreCase = ignoreCase) }
+	fun List<String>.anyEquals(other: String, ignoreCase: Boolean = false): Boolean =
+		any { it.equals(other, ignoreCase = ignoreCase) }
+	fun List<String>.anyContains(other: String, ignoreCase: Boolean = false): Boolean =
+		any { it.contains(other, ignoreCase = ignoreCase) }
 
 	fun <T> List<T>.nextAfter(after: T, skip: Int = 1): T? {
-		val index = this.indexOf(after)
+		val index = indexOf(after)
 		if(index == -1) return null
-		return this.getOrNull(index + skip)
+		return getOrNull(index + skip)
 	}
 }
