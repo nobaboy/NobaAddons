cb9702cf8cf571f72745898e2ac11bb4f8bff9a7
celeste <hi@celestialfault.dev>
2025-04-22 00:05:45 -0600

    convert this

--- src/main/kotlin/me/nobaboy/nobaaddons/features/crimsonisle/AnnounceVanquisher.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/features/crimsonisle/AnnounceVanquisher.kt
@@ -3,7 +3,7 @@ package me.nobaboy.nobaaddons.features.crimsonisle
 import me.nobaboy.nobaaddons.api.skyblock.SkyBlockAPI
 import me.nobaboy.nobaaddons.config.NobaConfig
 import me.nobaboy.nobaaddons.events.impl.chat.ChatMessageEvents
-import me.nobaboy.nobaaddons.repo.Repo.fromRepo
+import me.nobaboy.nobaaddons.repo.Repo
 import me.nobaboy.nobaaddons.utils.LocationUtils
 import me.nobaboy.nobaaddons.utils.StringUtils
 import me.nobaboy.nobaaddons.utils.StringUtils.cleanFormatting
@@ -12,7 +12,7 @@ object AnnounceVanquisher {
 	private val config get() = NobaConfig.crimsonIsle.announceVanquisher
 	private val enabled: Boolean get() = config.enabled && SkyBlockAPI.inSkyBlock
 
-	private val VANQUISHER_SPAWN_MESSAGE by "A Vanquisher is spawning nearby!".fromRepo("crimson_isle.vanquisher_spawn")
+	private val VANQUISHER_SPAWN_MESSAGE by Repo.string("crimson_isle.vanquisher_spawn", "A Vanquisher is spawning nearby!")
 
 	fun init() {
 		ChatMessageEvents.CHAT.register { (message) -> onChatMessage(message.string.cleanFormatting()) }
