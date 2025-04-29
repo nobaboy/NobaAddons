9543e2d3e2a24ff047907f2dff811030e709cde7
nobaboy <84668101+nobaboy@users.noreply.github.com>
2025-04-29 22:43:11 +0300

    Add missing code attributions (#136)

--- src/main/kotlin/me/nobaboy/nobaaddons/utils/math/PolynomialFitter.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/utils/math/PolynomialFitter.kt
@@ -2,6 +2,11 @@ package me.nobaboy.nobaaddons.utils.math
 
 import kotlin.math.pow
 
+/**
+ * This is taken from SkyHanni, which is licensed under the LGPL-2.1.
+ *
+ * [Original source](https://github.com/hannibal002/SkyHanni/blob/beta/src/main/java/at/hannibal2/skyhanni/utils/PolynomialFitter.kt#L6-L30)
+ */
 class PolynomialFitter(private val degree: Int) {
 	private val xMatrix = ArrayList<DoubleArray>()
 	private val yMatrix = ArrayList<DoubleArray>()
