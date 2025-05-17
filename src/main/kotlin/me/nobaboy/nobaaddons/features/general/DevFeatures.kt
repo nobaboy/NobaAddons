ae9ef01c5e58d8df379f322b3295f8c009e8c4d8
celeste <hi@celestialfault.dev>
2025-05-16 20:41:56 -0600

    back to json this goes

--- src/main/kotlin/me/nobaboy/nobaaddons/features/general/DevFeatures.kt
+++ src/main/kotlin/me/nobaboy/nobaaddons/features/general/DevFeatures.kt
@@ -1,5 +1,7 @@
 package me.nobaboy.nobaaddons.features.general
 
+import com.google.gson.GsonBuilder
+import com.mojang.serialization.JsonOps
 import me.nobaboy.nobaaddons.core.PersistentCache
 import me.nobaboy.nobaaddons.mixins.accessors.HandledScreenAccessor
 import me.nobaboy.nobaaddons.utils.MCUtils
@@ -7,25 +9,22 @@ import me.nobaboy.nobaaddons.utils.annotations.UntranslatedMessage
 import me.nobaboy.nobaaddons.utils.chat.ChatUtils
 import net.minecraft.client.gui.screen.ingame.HandledScreen
 import net.minecraft.item.ItemStack
-import net.minecraft.nbt.NbtOps
-import net.minecraft.nbt.visitor.StringNbtWriter
 import net.minecraft.screen.ScreenHandler
 import org.lwjgl.glfw.GLFW
 
 object DevFeatures {
+	private val gson = GsonBuilder().setPrettyPrinting().create()
+
 	@JvmStatic
 	@OptIn(UntranslatedMessage::class)
 	fun copyCurrentHoveredInventorySlot(screen: HandledScreen<out ScreenHandler>) {
 		val slot = (screen as HandledScreenAccessor).focusedSlot ?: return
 		if(slot.stack.isEmpty) return
 
-		MCUtils.copyToClipboard(buildTextToCopy(slot.stack))
+		// it's easier to just dump the item to json than to try to use any of minecraft's nbt pretty printing
+		val encoded = ItemStack.CODEC.encodeStart(MCUtils.player!!.registryManager.getOps(JsonOps.INSTANCE), slot.stack).orThrow
+		MCUtils.copyToClipboard(gson.toJson(encoded))
 		ChatUtils.addMessage("Copied item data to clipboard")
-	}
-
-	private fun buildTextToCopy(item: ItemStack): String {
-		val encoded = ItemStack.CODEC.encodeStart(MCUtils.player!!.registryManager.getOps(NbtOps.INSTANCE), item).orThrow
-		return StringNbtWriter().apply(encoded)
 	}
 
 	@JvmStatic
